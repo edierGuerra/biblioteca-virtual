22a7703d254bd561f6d3266b197279383afcd01e
Edier55 <edierguerra55@gmail.com>
2025-01-04 15:18:30 -0500

    feat(db): agrega utilidades para interactuar con la base de datos
    - Agrega una funcion para crear los modelos en la base de datos
    - Agrega una función que funciona como una fabrica de seciones

---
+++ database/utils.py
@@ -0,0 +1,17 @@
+from database.db import SessionLocal,Base,engine
+from database.models import book,user
+#Fabrica de sessiones
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+# Función para crear todas las tablas
+def create_db():
+    try:
+        Base.metadata.create_all(bind=engine)
+        print("¡Tablas creadas exitosamente!")
+    except Exception as e:
+        print(f"Error al crear las tablas: {e}")
