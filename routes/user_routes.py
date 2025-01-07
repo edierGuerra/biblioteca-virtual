e2144e5cc517fba2b3923314d5dddd0ac04445c7
Edier55 <edierguerra55@gmail.com>
2025-01-07 12:26:39 -0500

    feat(routes): añade ruta que renderiza formulario
    - Configura la plantilla para renderizar el formulario de registro.
    - Configura la ruta "/user/create/" para capturar los datos del formulario.

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,12 +1,22 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,Form
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse 
+from fastapi.templating import Jinja2Templates 
 from sqlalchemy.orm import Session 
 from database.utils import get_db
-from services.users_services import create_user
+from services.users_services import create_user,validate_email
 
 route = APIRouter(prefix="/user")
 
+template = Jinja2Templates("templates")
+
 @route.post("/create/")
-def create_users(name:str,email:str,password:str,db:Session = Depends(get_db)) -> dict:
+def create_users(
+    name:str = Form(...),
+    email:str = Form(...),
+    password:str = Form(...),
+    db:Session = Depends(get_db)
+    ):
     """
     # Crear usuario
     crea un usuario en la base de datos.
@@ -15,16 +25,28 @@ def create_users(name:str,email:str,password:str,db:Session = Depends(get_db)) -
     - email: Correo del usuario.
     - password: Contraseña del usuario.
     ## Retorna:
-    O con los datos del usuario.
+    Diccionario con los datos del usuario.
     """
     try:
-        user_data = create_user(
-            user_name=name,
-            user_email=email,
-            user_password=password,
-            db=db
-        )
-        return user_data
+        user_valid = validate_email(email,db)
+        if user_valid == True:
+            return {"message":"El correo ingresado ya se encutra registrado"}
+        elif user_valid == False:
+            user_data = create_user(
+                user_name=name,
+                user_email=email,
+                user_password=password,
+                db=db
+            )
+            return user_data
+        return {}
     except Exception as e: 
         print("A ocurrido un error inesperado: ", e)
-    
+
+@route.get("/logout",response_class=HTMLResponse)
+def render_logout(request:Request):
+    """
+    # Formulario de registro usuario
+    Renderiza el formulario de html para que el usuario pudea ingresar sus datos
+    """
+    return template.TemplateResponse("pages/user_logout.html",{"request":request})
